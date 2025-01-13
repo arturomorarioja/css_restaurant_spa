30e568dc3a36f31a54845172df64242fdb32b303
Arturo Mora-Rioja <arturomora@gmail.com>
2025-01-13 08:34:31 +0100

    Page title updated

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@
 document.querySelectorAll('nav > ul > li > a').forEach(menuOption => {
     menuOption.addEventListener('click', function() {
         
-        // Only the clicked menu option is displayed as selected
+        // Only the clicked menu option is displayed as selected.
         document.querySelectorAll(`nav > ul > li > a`).forEach(option => {
             option.classList.remove('selected');
         });
         this.classList.add('selected');
+        
+        // The page title is updated accordingly
+        document.title = `${this.innerText} | Restaurant KEA`;
         
         // The corresponding page is shown. The rest are hidden
         const dataTarget = this.getAttribute('data-target');        
