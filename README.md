3054097cb5a4065a3bb64301de9f3efd635f0c37
Arturo Mora-Rioja <arturomora@gmail.com>
2025-01-13 08:31:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# KEA Restaurant
+SPA version of the [KEA Restaurant exercise](https://github.com/arturomorarioja/kea_css_restaurant_solution).
+
+## Tools
+JavaScript / CSS3 / HTML5
+
+## Author:
+Arturo Mora-Rioja
---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * SPA handling
+ */
+document.querySelectorAll('nav > ul > li > a').forEach(menuOption => {
+    menuOption.addEventListener('click', function() {
+        
+        // Only the clicked menu option is displayed as selected
+        document.querySelectorAll(`nav > ul > li > a`).forEach(option => {
+            option.classList.remove('selected');
+        });
+        this.classList.add('selected');
+        
+        // The corresponding page is shown. The rest are hidden
+        const dataTarget = this.getAttribute('data-target');        
+        document.querySelector(`#${dataTarget}`).classList.remove('hidden');
+        document.querySelectorAll(`.page:not(#${dataTarget})`).forEach(page => {
+            page.classList.add('hidden');
+        });
+    });
+});
